22c7e2117cbf483810878fa15e32cdd576ebe39c
IrisAndMimi <my1ris227@outlook.com>
2018-04-20 09:48:22 +0800

    first commit

---
+++ MVC_0411/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_0411.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "關魚大內容啦啦啦";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "聯絡大內容啦啦啦";
+
+            return View();
+        }
+        public ActionResult Progress()
+        {
+            ViewBag.Message = "病程大內容啦啦啦";
+
+            return View();
+        }
+        public ActionResult html5()
+        {
+            ViewBag.Message = "啥";
+
+            return View();
+        }
+    }
+}
---
+++ MVC_0411/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_0411.MvcApplication" Language="C#" %>
